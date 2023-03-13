caa4d98c613e4a82f45152280b4a95ec82cfd7af
Amit Choudhary <amitamitmm@gmail.com>
2023-03-13 13:35:09 +0530

    This is third commit from vs code

--- src/component/Message.tsx
+++ src/component/Message.tsx
@@ -3,7 +3,7 @@
 function Message(){
     const name = 'Choudhary';
     if(name)
-        return <h1>Hello hhh. {name}</h1>
+        return <h1>Hello hhhh. {name}</h1>
     return <h1>Hello World!</h1>
 }
 export default Message;
